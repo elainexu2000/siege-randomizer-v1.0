05b0623c9012fe1675c7ad015f943d2c20cf1b71
elainexu2000 <elainexu2000@gmail.com>
2023-05-09 01:31:05 -0700

    Minor modifications to variable names to improve consistency

--- operators/Operator.py
+++ operators/Operator.py
@@ -3,10 +3,10 @@ from random import choice
 class Operator:
     """ Attributes:
         name: operator name
-        side: in ["Attack", "Defend"]
-        gadgets: set of two available gadgets
-        primary: set of all available primary weapons
-        secondary: set of all available secondary weapons
+        side: "Attack" or "Defend"
+        gadgets: set of all available gadgets
+        primaries: set of all available primary weapons
+        secondaries: set of all available secondary weapons
     """
     def __init__(self, name, side):
         """
@@ -15,8 +15,8 @@ class Operator:
         self.name = name
         self.side = side
         self.gadgets = []
-        self.primary = []
-        self.secondary = []
+        self.primaries = []
+        self.secondaries = []
     
     def __str__(self):
         return self.name
@@ -29,20 +29,20 @@ class Operator:
         Precondition: weapon is a valid Weapon object, is_primary = True if this weapon is a primary weapon
         """
         if is_primary:
-            self.primary.append(weapon)
+            self.primaries.append(weapon)
         else:
-            self.secondary.append(weapon)
+            self.secondaries.append(weapon)
     
     def get_random_loadout(self):
         loadout = {}
-        primary = choice(self.primary)
+        primary = choice(self.primaries)
         loadout["Primary"] = str(primary)
         loadout["Primary Grip"] = choice(primary.grips)
         loadout["Primary Barrel"] = choice(primary.barrels)
         loadout["Primary Scope"] = choice(primary.scopes)
         loadout["Primary Under Barrel"] = choice(primary.under_barrels)
         
-        secondary = choice(self.secondary)
+        secondary = choice(self.secondaries)
         loadout["Secondary"] = str(secondary)
         loadout["Secondary Grip"] = choice(secondary.grips)
         loadout["Secondary Barrel"] = choice(secondary.barrels)
@@ -51,13 +51,6 @@ class Operator:
         loadout["Gadget"] = str(choice(self.gadgets))
         return loadout
     
-    def get_random_primary(self):
-        primary = choice(self.primary)
-        primary_grip = choice(primary.grips)
-        primary_barrel = choice(primary.barrels)
-        primary_scope = choice(primary.scopes)
-        primary_under_barrel = choice(primary.under_barrels)
-        return primary, primary_grip, primary_barrel, primary_scope, primary_under_barrel
 
         
 
