b9fdcdf043a47f33d14a6f59ce72dc1994a7901c
elainexu2000 <elainexu2000@gmail.com>
2023-06-12 22:02:05 -0700

    Added new test cases

--- testing.py
+++ testing.py
@@ -1,6 +1,17 @@
 from random import choice
 from operators.attacker import *
 from operators.defender import *
+from operators.r6_operator import Operator
+from gadgets.gadget import *
+from weapons.assault_rifle import *
+from weapons.handgun import *
+from weapons.light_machine_gun import *
+from weapons.machine_pistol import *
+from weapons.marksman_rifle import *
+from weapons.shield import *
+from weapons.shotgun import *
+from weapons.submachine_gun import *
+from weapons.hand_cannon import *
 
 # Global:
 ATTACKERS = tuple([cls() for cls in Attacker.__subclasses__()])
@@ -52,7 +63,53 @@ if __name__ == "__main__":
     attacker = Attacker("name")
     print(type(glaz).__name__)
     print(type(attacker).__name__)
-    """
+    
     for op in ALL_OPERATORS:
         print(op.image_path)
+    """
+    a = Ash()
+    print(a.get_random_loadout())
+    """{
+        'Operator': {
+            'Name': 'Ash', 
+            'Image': '../assets/operators/icons/Ash.png'
+        }, 
+        'Gadget': {
+            'Name': 'Breach Charge', 
+            'Image': '../assets/gadgets/attacker_gadgets/Breach_Charge.png'
+        }, 
+        'Primary': {
+            'Name': 'G36C', 
+            'Image': '../assets/weapons/assault_rifles/G36C.png', 
+            'Scope': {
+                'Name': 'Holographic A', 
+                'Image': '../assets/sights/Holo_A.png'
+            }, 
+            'Barrel': {
+                'Name': 'Holographic A', 
+                'Image': '../assets/sights/Holo_A.png'
+            }, 
+            'Under Barrel': {
+                'Name': 'Laser', 
+                'Image': '../assets/under_barrels/Laser.png'
+            }
+        }, 
+        'Secondary': {
+            'Name': 'M45 MEUSOC', 
+            'Image': '../assets/weapons/handguns/M45_MEUSOC.png', 
+            'Scope': {
+                'Name': 'None', 
+                'Image': '../assets/None.png'
+            }, 
+            'Barrel': {
+                'Name': 'None', 
+                'Image': '../assets/None.png'
+            }, 
+            'Under Barrel': {
+                'Name': 'Laser', 
+                'Image': '../assets/under_barrels/Laser.png'
+            }
+        }
+    }
+    """
     
