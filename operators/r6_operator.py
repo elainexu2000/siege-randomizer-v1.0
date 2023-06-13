3e92ee9e861b70b6cbad794a388d4530da7ca46f
elainexu2000 <elainexu2000@gmail.com>
2023-06-12 22:17:08 -0700

    Removed code used for testing purposes

--- operators/r6_operator.py
+++ operators/r6_operator.py
@@ -38,6 +38,17 @@ class Operator:
             self.secondaries.append(weapon)
 
     def create_subdictionary(loadout, obj, entity_name):
+        """Adds obj.name and obj.image_path to loadout
+        Precondition: loadout is a dictionary, obj is a non-null Object with attributes 'name' and 'image_path' 
+            (Operator, Gadget, Weapon, Scope, Barrel, Under_Barrel), entity_name is a string which serves as a key in loadout
+        Example: create_subdictionary(loadout = {}, Ash(), "Operator")
+            loadout = {
+                "Operator": {
+                    "Name": "Ash",
+                    "Image": "../asset/operators/Ash.png"
+                    }
+                }
+        """
         loadout[entity_name] = {}
         loadout[entity_name]["Name"] = obj.name
         loadout[entity_name]["Image"] = obj.image_path
@@ -46,14 +57,11 @@ class Operator:
         name = "Primary" if is_primary else "Secondary"
         Operator.create_subdictionary(loadout, weapon, name)
         scope_class = choice(weapon.scopes)
-        scope = scope_class()
-        Operator.create_subdictionary(loadout[name], scope, "Scope")
+        Operator.create_subdictionary(loadout[name], scope_class(), "Scope")
         barrel_class = choice(weapon.barrels)
-        barrel = barrel_class()
-        Operator.create_subdictionary(loadout[name], barrel, "Barrel")
+        Operator.create_subdictionary(loadout[name], barrel_class(), "Barrel")
         under_barrel_class = choice(weapon.under_barrels)
-        under_barrel = under_barrel_class()
-        Operator.create_subdictionary(loadout[name], under_barrel, "Under Barrel")
+        Operator.create_subdictionary(loadout[name], under_barrel_class(), "Under Barrel")
 
     def get_random_loadout(self):
         """
@@ -63,17 +71,8 @@ class Operator:
         Operator.create_subdictionary(loadout, self, "Operator")
         gadget = choice(self.gadgets)
         Operator.create_subdictionary(loadout, gadget, "Gadget")
-
         primary = choice(self.primaries)
         Operator.add_randomized_weapon(loadout, primary, is_primary=True)
         secondary = choice(self.secondaries)
         Operator.add_randomized_weapon(loadout, secondary, is_primary=False)
         return loadout
-
-if __name__ == '__main__':
-    r = Operator("Recruit", "Attack")
-    
-    loadout = {}
-    Operator.create_subdictionary(loadout, r, "Primary")
-    Operator.create_subdictionary(loadout["Primary"], r, "Scope")
-    print(loadout)
