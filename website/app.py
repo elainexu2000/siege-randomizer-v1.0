742df976e43fe2ffa9fad5af505364641ccc4628
elainexu2000 <elainexu2000@gmail.com>
2023-06-27 14:37:44 -0700

    Modified JS function structure

--- website/app.py
+++ website/app.py
@@ -1,7 +1,7 @@
 import sys
 from flask import Flask, redirect, render_template, request, url_for
 sys.path.append("../")
-from siege_randomizer import get_random_operator
+from siege_randomizer import get_random_operator, get_random_attacker, get_random_defender
 
 app = Flask(__name__)
 
@@ -9,8 +9,18 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/generate')
-def generate():
+@app.route('/generate_attacker')
+def generate_attacker():
+    generated_string = get_random_attacker().get_random_loadout()
+    return generated_string
+
+@app.route('/generate_defender')
+def generate_defender():
+    generated_string = get_random_defender().get_random_loadout()
+    return generated_string
+
+@app.route('/generate_all')
+def generate_all():
     generated_string = get_random_operator().get_random_loadout()
     return generated_string
 
